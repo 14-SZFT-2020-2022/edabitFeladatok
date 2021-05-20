89ded9eace3880b7be587f19fd34956b48c973c8
pepe1973 <bodrogipr@gmail.com>
2021-05-20 11:07:34 +0200

    Első kommit

---
+++ EdabitFeladatok/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitFeladatok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // VeryEasy.Sum(3, 2);
+            // VeryEasy.Sum(-3, -6);
+            // VeryEasy.Addition(0);
+            // VeryEasy.Addition(9);
+            // VeryEasy.TriArea(10, 12);
+            // VeryEasy.TriArea(3, 5);
+            // VeryEasy.Remainder(3, 5);
+            // VeryEasy.Remainder(5, 3);
+            VeryEasy.LessThan(22, 15);
+            VeryEasy.LessThan(83, 34);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ EdabitFeladatok/VeryEasy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitFeladatok
+{
+    class VeryEasy
+    {
+        public static int Sum(int a, int b)
+        {
+            int osszeg = a + b;
+
+            Console.WriteLine($"Az első szám: {a}, és a második szám: {b} összege: {osszeg}");
+
+            return osszeg;
+        }
+
+        public static int Addition(int a)
+        {
+            int kovetkezo = a + 1;
+
+            Console.WriteLine($"A bekért szám: {a} utáni első szám: {kovetkezo}");
+
+            return kovetkezo;
+        }
+
+        public static double TriArea(int alap, int magassag)
+        {
+            double terulet = alap * magassag / 2.0d;
+
+            Console.WriteLine($"A háromszög alapja: {alap}, magassága: {magassag} és területe: {terulet}");
+
+            return terulet;
+        }
+
+        public static int Remainder(int a, int b)
+        {
+            int maradek = a % b;
+            int hanyados = a / b;
+            string kimenet = $"Első szám: {a}, második szám: {b}, maradék: {maradek}, " +
+                $"osztás: {a} = {hanyados} * {b} + {maradek}.";
+
+            Console.WriteLine(kimenet);
+
+            return maradek;
+        }
+
+        public static bool LessThan(int a, int b)
+        {
+            int osszeg = a + b;
+            
+            if (osszeg < 100)
+            {
+                Console.WriteLine($"Igaz. {a} + {b} = {osszeg} < 100");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Hamis. {a} + {b} = {osszeg} > 100");
+                return false;
+            }
+        }
+    }
+}
